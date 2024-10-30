719d7d500c5695adba432f8b7c162cc7925c75e1
Philix27 <philixbob@gmail.com>
2024-10-30 08:14:09 +0100

    clean up

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -13,10 +13,28 @@ import "react-native-reanimated";
 import { useColorScheme } from "@/components";
 import { AppUtils } from "@/utils";
 
+import { useWalletConnectModal } from "@walletconnect/modal-react-native";
+
 export {
   // Catch any errors thrown by the Layout component.
   ErrorBoundary,
 } from "expo-router";
+
+import "@walletconnect/react-native-compat";
+import { WalletConnectModal } from "@walletconnect/modal-react-native";
+
+const projectId = "YOUR_PROJECT_ID";
+
+const providerMetadata = {
+  name: "YOUR_PROJECT_NAME",
+  description: "YOUR_PROJECT_DESCRIPTION",
+  url: "https://your-project-website.com/",
+  icons: ["https://your-project-logo.com/"],
+  redirect: {
+    native: "YOUR_APP_SCHEME://",
+    universal: "YOUR_APP_UNIVERSAL_LINK.com",
+  },
+};
 
 export const unstable_settings = {
   // Ensure that reloading on `/modal` keeps a back button present.
@@ -52,9 +70,19 @@ export default function RootLayout() {
 
 function RootLayoutNav() {
   const colorScheme = useColorScheme();
+  const {
+    open,
+    isConnected,
+    provider,
+    address: wcAddress,
+  } = useWalletConnectModal();
 
   return (
     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+      <WalletConnectModal
+        projectId={projectId}
+        providerMetadata={providerMetadata}
+      />
       <Stack>
         <Stack.Screen
           name="index"
